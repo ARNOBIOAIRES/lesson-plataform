d37cb557223917689e91ef9d9efd33aeda731e6c
Arnobio <arnobioaires2@outlook.com>
2024-07-28 22:44:59 -0300

    feat: create pattern in global and tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function PageHome() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className='bg-background'>
    Home
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,15 @@ const config: Config = {
   
   theme: {
     extend: {
+      colors: {
+        text: 'var(--color-text)',
+        error: 'var(--color-error)',
+        paper: 'var(--color-paper)',
+        primary: 'var(--color-primary)',
+        background: 'var(--color-background)',
+        'primary-contrast': 'var(--color-primary-contrast)'
+  
+      }
     
     },
   },
